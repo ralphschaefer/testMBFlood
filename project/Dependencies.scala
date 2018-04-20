3c567c9c18e309ca4320df3aa12796ac83089ec2
Ralph Schäfer <ralph.schaefer@emnify.com>
2018-04-20 08:58:58 +0200

    logback / roundrobin

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,6 +11,7 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-testkit" % akkaVersion % Test,
     "org.scalatest" %% "scalatest" % "3.0.1",
     "ch.qos.logback" % "logback-classic" % "1.1.3",
+    "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
     "com.github.jknack" % "handlebars" % "4.0.6",
     "com.github.jknack" % "handlebars-jackson2" % "4.0.6",
     "org.json4s" %% "json4s-jackson" % "3.5.3",
--- startup.sh
+++ startup.sh
@@ -5,6 +5,6 @@ tmux new-session java -jar ${DEST}mbFloodSeedNode.jar \; \
      split-window -v java -jar modules/module1/${DEST}module1.jar \; \
      split-window -h java -jar modules/module1/${DEST}module1.jar \; \
      select-pane -t 0 \; \
-     split-window -h java -jar modules/module2/${DEST}module2.jar \; 
+     split-window -h java -jar modules/module2/${DEST}module2.jar \;
 #     select-pane -t 0 \;
      	
